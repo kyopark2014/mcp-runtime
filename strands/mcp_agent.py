b6f76120240f21497f16a7e5e8e192d94f7d7d28
John Park <kyopark2014@gmail.com>
2025-08-04 08:30:04 +0900

    update

---
+++ strands/mcp_agent.py
@@ -0,0 +1,11 @@
+from strands import Agent
+from strands.tools.mcp import MCPClient
+from mcp import stdio_client, StdioServerParameters
+
+aws_docs_client = MCPClient(
+    lambda: stdio_client(StdioServerParameters(command="uvx", args=["awslabs.aws-documentation-mcp-server@latest"]))
+)
+
+with aws_docs_client:
+   agent = Agent(tools=aws_docs_client.list_tools_sync())
+   response = agent("Tell me about Amazon Bedrock and how to use it with Python")
