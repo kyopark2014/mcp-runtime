04421ea32b1bd16c8f530f1ddcf4127d2361b0c7
John Park <kyopark2014@gmail.com>
2025-09-07 10:38:09 +0900

    update

--- runtime/use-aws/test_mcp_remote.py
+++ runtime/use-aws/test_mcp_remote.py
@@ -26,7 +26,7 @@ def create_cognito_bearer_token(config):
     """Get a fresh bearer token from Cognito"""
     try:
         cognito_config = config['cognito']
-        region = cognito_config['region']
+        region = config['region']
         client_id = cognito_config['client_id']
         username = cognito_config['test_username']
         password = cognito_config['test_password']
